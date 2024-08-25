0524787f91a11a494b7be9599c77fcce95a3bda0
eashan1809 <104688928+eashan1809@users.noreply.github.com>
2024-08-26 01:32:00 +0530

    createProduct

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,22 +1,73 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
-import AdminMenu from '../../components/Layout/AdminMenu'
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+import { Select } from "antd";
+const { Option } = Select;
 
 const CreateProduct = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      // toast.error("Something wwent wrong in getting catgeory");
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Dashboard - Create Product"}>
-    <div className="container-fluid m-3 p-3">
-      <div className="row">
-        <div className="col-md-3">
-          <AdminMenu />
-        </div>
-        <div className="col-md-9">
-          <h1>Create  Product </h1>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9 ">
+            <h1>Create Product </h1>
+            <div className=" m-2">
+            <Select
+                bordered={false}
+                placeholder="Select a category"
+                size='large'
+                showSearch
+                className="form-select mb-2 "
+               
+                onChange={(value) => {
+                  setCategory(value);
+                }}
+              >
+                {categories?.map((c) => (
+                  <Option key={c._id} value={c._id}>
+                    {c.name}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-  )
-}
+    </Layout>
+  );
+};
 
-export default CreateProduct
+export default CreateProduct;
